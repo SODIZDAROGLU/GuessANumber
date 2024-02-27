34580ae2445e4cfa6590667a1db83fe5ebd7c8bf
sodizdaroglu <americanlaz@gmail.com>
2024-02-27 09:41:23 -0500

    update10

--- script.js
+++ script.js
@@ -25,9 +25,22 @@ function onSpeak(e){
 }
 
 function writeMessage(msg){
+    const numberWords = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+        'ten': 10
+    };
+    const numericMsg = numberWords.hasOwnProperty(msg.toLowerCase()) ? numberWords[msg.toLowerCase()] : msg;
     msgEl.innerHTML = ` 
     <div>You said:</div>
-    <span class="box">${msg}</span>
+    <span class="box">${numericMsg}</span>
    `;
 }
 
@@ -67,36 +80,36 @@ function wordToNumber(input) {
         console.log("Input is not a string:", input);
         let lowerCaseWord = input.toLowerCase();
         
-
-  //  console.log("Lowercase word:", lowerCaseWord);
-    switch (lowerCaseWord) {
-        case 'one':
-            return 1;
-        case 'two':
-            return 2;
-        case 'three':
-            return 3;
-        case 'four':
-            return 4;
-        case 'five':
-            return 5;
-        case 'six':
-            return 6;
-        case 'seven':
-            return 7;
-        case 'eight':
-            return 8;
-        case 'nine':
-            return 9;
-        case 'ten':
-            return 10;
-        default:
-            return input;
+        switch (lowerCaseWord) {
+            case 'one':
+                return 1;
+            case 'two':
+                return 2;
+            case 'three':
+                return 3;
+            case 'four':
+                return 4;
+            case 'five':
+                return 5;
+            case 'six':
+                return 6;
+            case 'seven':
+                return 7;
+            case 'eight':
+                return 8;
+            case 'nine':
+                return 9;
+            case 'ten':
+                return 10;
+            default:
+                return input;
+        }
     }
-}
-return input;
-   
+    return input;
+       
 };
+
+
 
 function getRandomNumber(){
     return Math.floor(Math.random() * 100) + 1
